66a7a11543698c7295c34e27670ddd029fb6377c
vjhwong <victor.ju.hin.wong@gmail.com>
2023-05-29 11:18:58 +0200

    Finished getComputerChoice

---
+++ script.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return "Rock";
+    case 1:
+      return "Paper";
+    case 2:
+      return "Scissor";
+  }
+}
+
+console.log(getComputerChoice());
