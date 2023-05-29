1666e2f81f19b108d079cf6a9887215b1324d9fd
vjhwong <victor.ju.hin.wong@gmail.com>
2023-05-29 12:44:52 +0200

    Completed playRound and game

--- script.js
+++ script.js
@@ -6,8 +6,39 @@ function getComputerChoice() {
     case 1:
       return "Paper";
     case 2:
-      return "Scissor";
+      return "Scissors";
   }
 }
 
-console.log(getComputerChoice());
+let formatString = (string) =>
+  string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+
+function playRound(playerSelection, computerSelection) {
+  const formattedPlayerSelection = formatString(playerSelection);
+  const winMessage = `You win! ${formattedPlayerSelection} beats ${computerSelection}`;
+  const lossMessage = `Ỳou lose! ${computerSelection} beats ${formattedPlayerSelection}`;
+  const drawMessage = `No winner, both you and the computer played ${computerSelection}`;
+
+  if (formattedPlayerSelection === computerSelection) return drawMessage;
+
+  switch (formattedPlayerSelection) {
+    case "Rock":
+      if (computerSelection === "Scissors") return winMessage;
+      else if (computerSelection === "Paper") return lossMessage;
+    case "Paper":
+      if (computerSelection === "Rock") return winMessage;
+      else if (computerSelection === "Scissors") return lossMessage;
+    case "Scissors":
+      if (computerSelection === "Paper") return winMessage;
+      else if (computerSelection === "Rock") return lossMessage;
+  }
+}
+
+function game() {
+  for (let index = 0; index < 10; index++) {
+    let playerSelection = prompt("Enter 'Rock', 'Paper' or 'Scissors'");
+    let computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+  }
+}
+game();
